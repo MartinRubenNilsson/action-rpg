b49fbce35ca62100fa14065af93702880c114780
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2023-08-07 22:01:26 +0200

    Open SFML window

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
-	std::cout << "Hello, SFML!" << std::endl;
+	sf::Window window(sf::VideoMode(800, 600), "Hello, SFML!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+	return 0;
 }
