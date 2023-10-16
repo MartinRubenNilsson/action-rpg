61ebf00371f3ffdad7d9d984e73c0b09dc544f51
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2023-10-16 22:34:43 +0200

    Add some furniture

---
+++ output/assets/tiled/tilesets/retro_interior/TopDownHouse_FurnitureState1.tsx
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TopDownHouse_FurnitureState1" tilewidth="16" tileheight="16" tilecount="234" columns="13">
+ <image source="TopDownHouse_FurnitureState1.png" width="208" height="288"/>
+ <tile id="121">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.90909" y="9.09091" width="8.09091" height="4.72727"/>
+  </objectgroup>
+ </tile>
+ <tile id="122">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="9.09091" width="8.72727" height="4.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="123">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="5.36364" y="7.72727" width="6.18182" height="6.18182">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="148">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.18182" y="0" width="6.63636" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="149">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="150">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="151">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0.0909091" y="0.181818" width="8.81818" height="15.8182"/>
+  </objectgroup>
+ </tile>
+</tileset>
