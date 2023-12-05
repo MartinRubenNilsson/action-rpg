7b6ebff77fc018d41d77544e53901fdca2501eca
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2023-12-05 21:14:01 +0100

    Add sprite hearts as health bar

--- ui_textbox_presets.h
+++ ui_textbox_presets.h
@@ -10,14 +10,13 @@ namespace ui
 
 	struct Textbox
 	{
+		std::string next_textbox; // name of textbox preset
 		std::string text; // RML
 		std::string sprite;
 		std::string opening_sound; // name of sound event
-		// std::string closing_sound; // TODO: implement
 		std::string typing_sound = TEXTBOX_TYPING_SOUND_DEFAULT;
 		float typing_speed = TEXTBOX_TYPING_SPEED_DEFAULT; // in CPS, 0 = instant
 		// bool auto_close = false; // TODO: implement
-		std::string next_textbox; // name of textbox preset
 		// bool sprite_to_left = false; // TODO: implement
 	};
 
