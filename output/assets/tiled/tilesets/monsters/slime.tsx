b0f4d3a1a7fb1d26602ac1742fd36cb38a2ea3a8
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2023-12-10 16:41:20 +0100

    Add slime tileset

---
+++ output/assets/tiled/tilesets/monsters/slime.tsx
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="slime" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="slime.png" width="128" height="128"/>
+ <tile id="0" type="d">
+  <objectgroup draworder="index" id="3">
+   <object id="8" x="9" y="9" width="14" height="14">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="4" type="r">
+  <animation>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="8" type="u">
+  <animation>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="11" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="12" type="l">
+  <animation>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="15" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
