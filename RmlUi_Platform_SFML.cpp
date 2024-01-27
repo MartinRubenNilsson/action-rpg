b042f387edb04e8232e83335c1aff2f48ddcb350
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-01-27 09:44:13 +0100

    Refactor

--- RmlUi_Platform_SFML.cpp
+++ RmlUi_Platform_SFML.cpp
@@ -1,31 +1,3 @@
-/*
- * This source file is part of RmlUi, the HTML/CSS Interface Middleware
- *
- * For the latest information, see http://github.com/mikke89/RmlUi
- *
- * Copyright (c) 2008-2010 CodePoint Ltd, Shift Technology Ltd
- * Copyright (c) 2019-2023 The RmlUi Team, and contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
-
 #include "RmlUi_Platform_SFML.h"
 #include <RmlUi/Core/Context.h>
 #include <RmlUi/Core/Input.h>
@@ -41,92 +13,92 @@ SystemInterface_SFML::SystemInterface_SFML()
 	cursors_valid &= cursor_default.loadFromSystem(sf::Cursor::Arrow);
 	cursors_valid &= cursor_move.loadFromSystem(sf::Cursor::SizeAll);
 	cursors_valid &= cursor_pointer.loadFromSystem(sf::Cursor::Hand);
-	cursors_valid &= cursor_resize.loadFromSystem(sf::Cursor::SizeTopLeftBottomRight) || cursor_resize.loadFromSystem(sf::Cursor::SizeAll);
+	cursors_valid &= cursor_resize.loadFromSystem(sf::Cursor::SizeTopLeftBottomRight)
+		|| cursor_resize.loadFromSystem(sf::Cursor::SizeAll);
 	cursors_valid &= cursor_cross.loadFromSystem(sf::Cursor::Cross);
 	cursors_valid &= cursor_text.loadFromSystem(sf::Cursor::Text);
 	cursors_valid &= cursor_unavailable.loadFromSystem(sf::Cursor::NotAllowed);
 }
 
-void SystemInterface_SFML::SetWindow(sf::RenderWindow* in_window)
-{
+void SystemInterface_SFML::SetWindow(sf::RenderWindow* in_window) {
 	window = in_window;
 }
 
-double SystemInterface_SFML::GetElapsedTime()
-{
+double SystemInterface_SFML::GetElapsedTime() {
 	return static_cast<double>(timer.getElapsedTime().asMicroseconds()) / 1'000'000.0;
 }
 
 void SystemInterface_SFML::SetMouseCursor(const Rml::String& cursor_name)
 {
-	if (cursors_valid && window)
-	{
-		sf::Cursor* cursor = nullptr;
-		if (cursor_name.empty() || cursor_name == "arrow")
-			cursor = &cursor_default;
-		else if (cursor_name == "move")
-			cursor = &cursor_move;
-		else if (cursor_name == "pointer")
-			cursor = &cursor_pointer;
-		else if (cursor_name == "resize")
-			cursor = &cursor_resize;
-		else if (cursor_name == "cross")
-			cursor = &cursor_cross;
-		else if (cursor_name == "text")
-			cursor = &cursor_text;
-		else if (cursor_name == "unavailable")
-			cursor = &cursor_unavailable;
-		else if (Rml::StringUtilities::StartsWith(cursor_name, "rmlui-scroll"))
-			cursor = &cursor_move;
+	if (!cursors_valid || !window) return;
 
-		if (cursor)
-			window->setMouseCursor(*cursor);
-	}
+	sf::Cursor* cursor = nullptr;
+	if (cursor_name.empty() || cursor_name == "arrow")
+		cursor = &cursor_default;
+	else if (cursor_name == "move")
+		cursor = &cursor_move;
+	else if (cursor_name == "pointer")
+		cursor = &cursor_pointer;
+	else if (cursor_name == "resize")
+		cursor = &cursor_resize;
+	else if (cursor_name == "cross")
+		cursor = &cursor_cross;
+	else if (cursor_name == "text")
+		cursor = &cursor_text;
+	else if (cursor_name == "unavailable")
+		cursor = &cursor_unavailable;
+	else if (Rml::StringUtilities::StartsWith(cursor_name, "rmlui-scroll"))
+		cursor = &cursor_move;
+
+	if (cursor)
+		window->setMouseCursor(*cursor);
 }
 
-void SystemInterface_SFML::SetClipboardText(const Rml::String& text_utf8)
-{
+void SystemInterface_SFML::SetClipboardText(const Rml::String& text_utf8) {
 	sf::Clipboard::setString(text_utf8);
 }
 
-void SystemInterface_SFML::GetClipboardText(Rml::String& text)
-{
+void SystemInterface_SFML::GetClipboardText(Rml::String& text) {
 	text = sf::Clipboard::getString();
 }
 
 bool RmlSFML::InputHandler(Rml::Context* context, const sf::Event& ev)
 {
-	bool result = true;
+	int key_modifier_state = RmlSFML::get_key_modifier_state();
 
 	switch (ev.type)
 	{
-	case sf::Event::MouseMoved: result = context->ProcessMouseMove(ev.mouseMove.x, ev.mouseMove.y, RmlSFML::GetKeyModifierState()); break;
-	case sf::Event::MouseButtonPressed: result = context->ProcessMouseButtonDown(ev.mouseButton.button, RmlSFML::GetKeyModifierState()); break;
-	case sf::Event::MouseButtonReleased: result = context->ProcessMouseButtonUp(ev.mouseButton.button, RmlSFML::GetKeyModifierState()); break;
-	case sf::Event::MouseWheelMoved: result = context->ProcessMouseWheel(float(-ev.mouseWheel.delta), RmlSFML::GetKeyModifierState()); break;
-	case sf::Event::MouseLeft: result = context->ProcessMouseLeave(); break;
-	case sf::Event::TextEntered:
-	{
-		Rml::Character character = Rml::Character(ev.text.unicode);
-		if (character == Rml::Character('\r'))
-			character = Rml::Character('\n');
-
-		if (ev.text.unicode >= 32 || character == Rml::Character('\n'))
-			result = context->ProcessTextInput(character);
+	case sf::Event::MouseMoved:
+		return context->ProcessMouseMove(ev.mouseMove.x, ev.mouseMove.y, key_modifier_state);
+	case sf::Event::MouseButtonPressed:
+		return context->ProcessMouseButtonDown(ev.mouseButton.button, key_modifier_state);
+	case sf::Event::MouseButtonReleased:
+		return context->ProcessMouseButtonUp(ev.mouseButton.button, key_modifier_state);
+	case sf::Event::MouseWheelMoved:
+		return context->ProcessMouseWheel(float(-ev.mouseWheel.delta), key_modifier_state);
+	case sf::Event::MouseLeft:
+		return context->ProcessMouseLeave();
+	case sf::Event::TextEntered: {
+		Rml::Character c = Rml::Character(ev.text.unicode);
+		if (c == Rml::Character('\r'))
+			c = Rml::Character('\n');
+		if (ev.text.unicode >= 32 || c == Rml::Character('\n'))
+			return context->ProcessTextInput(c);
+		else
+			return true;
 	}
-	break;
-	case sf::Event::KeyPressed: result = context->ProcessKeyDown(RmlSFML::ConvertKey(ev.key.code), RmlSFML::GetKeyModifierState()); break;
-	case sf::Event::KeyReleased: result = context->ProcessKeyUp(RmlSFML::ConvertKey(ev.key.code), RmlSFML::GetKeyModifierState()); break;
-	default: break;
+	case sf::Event::KeyPressed:
+		return context->ProcessKeyDown(RmlSFML::sfml_key_to_rml_key(ev.key.code), key_modifier_state);
+	case sf::Event::KeyReleased:
+		return context->ProcessKeyUp(RmlSFML::sfml_key_to_rml_key(ev.key.code), key_modifier_state);
+	default:
+		return true;
 	}
-
-	return result;
 }
 
-Rml::Input::KeyIdentifier RmlSFML::ConvertKey(sf::Keyboard::Key sfml_key)
+Rml::Input::KeyIdentifier RmlSFML::sfml_key_to_rml_key(sf::Keyboard::Key key)
 {
-	// clang-format off
-	switch (sfml_key)
+	switch (key)
 	{
 	case sf::Keyboard::A:         return Rml::Input::KI_A;
 	case sf::Keyboard::B:         return Rml::Input::KI_B;
@@ -211,25 +183,18 @@ Rml::Input::KeyIdentifier RmlSFML::ConvertKey(sf::Keyboard::Key sfml_key)
 	case sf::Keyboard::Space:     return Rml::Input::KI_SPACE;
 	case sf::Keyboard::Subtract:  return Rml::Input::KI_SUBTRACT;
 	case sf::Keyboard::Tab:       return Rml::Input::KI_TAB;
-	default: break;
+	default:                      return Rml::Input::KI_UNKNOWN;
 	}
-	// clang-format on
-
-	return Rml::Input::KI_UNKNOWN;
 }
 
-int RmlSFML::GetKeyModifierState()
+Rml::Input::KeyModifier RmlSFML::get_key_modifier_state()
 {
-	int modifiers = 0;
-
+	unsigned char modifiers = 0;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
 		modifiers |= Rml::Input::KM_SHIFT;
-
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) || sf::Keyboard::isKeyPressed(sf::Keyboard::RControl))
 		modifiers |= Rml::Input::KM_CTRL;
-
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LAlt) || sf::Keyboard::isKeyPressed(sf::Keyboard::RAlt))
 		modifiers |= Rml::Input::KM_ALT;
-
-	return modifiers;
+	return (Rml::Input::KeyModifier)modifiers;
 }
