1520d018ce22d192ebf1096a2eb17806010a80d6
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-02-11 11:49:40 +0100

    Refactor

--- textures.h
+++ textures.h
@@ -5,8 +5,6 @@ namespace textures
 	extern const std::filesystem::path ERROR_TEXTURE_PATH;
 	extern bool log_errors;
 
-	// The texture is loaded from file if it's not already in the cache.
-	// If the texture fails to load, an error texture is returned instead.
 	std::shared_ptr<sf::Texture> get(const std::filesystem::path& path);
 	void clear_cache();
 }
