81fdb3f42e808c38f6417d55defaf1f8ed9688ba
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-03-10 08:07:46 +0100

    Try to get rid of some cache misses while pathfinding

--- map_tilegrid.cpp
+++ map_tilegrid.cpp
@@ -76,14 +76,16 @@ namespace map
 
 	const sf::Vector2i _ALLOWED_MOVEMENT_DIRECTIONS[] =
 	{
-		sf::Vector2i(1, 0),
-		sf::Vector2i(-1, 0),
-		sf::Vector2i(0, 1),
-		sf::Vector2i(0, -1),
-		sf::Vector2i(1, 1),
-		sf::Vector2i(-1, 1),
-		sf::Vector2i(1, -1),
+		// The order of these directions has been chosen to minimize cache misses
+		// when iterating over the array while pathfinding, so don't change it.
 		sf::Vector2i(-1, -1),
+		sf::Vector2i( 0, -1),
+		sf::Vector2i( 1, -1),
+		sf::Vector2i(-1,  0),
+		sf::Vector2i( 1,  0),
+		sf::Vector2i(-1,  1),
+		sf::Vector2i( 0,  1),
+		sf::Vector2i( 1,  1),
 	};
 
 	TileGrid _grid;
