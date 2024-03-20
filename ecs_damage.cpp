be2fb4a39a1df92bf4372e73b141ff663c69278e
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-03-20 20:35:22 +0100

    Work on new damage system

--- ecs_damage.cpp
+++ ecs_damage.cpp
@@ -1,7 +1,27 @@
 #include "stdafx.h"
 #include "ecs_damage.h"
+#include "ecs_common.h"
 
 namespace ecs
 {
+	extern entt::registry _registry;
 
+	bool apply_damage(entt::entity entity, const Damage& damage)
+	{
+		if (!_registry.valid(entity)) return false;
+		std::string class_ = get_class(entity);
+		if (class_ == "player") {
+			//return apply_damage_to_player(entity, damage);
+			return false; //TODO
+		} else if (class_ == "grass") {
+			return apply_damage_to_grass(entity, damage);
+		}
+		return false;
+	}
+
+	bool apply_damage_to_grass(entt::entity entity, const Damage& damage)
+	{
+		//TODO
+		return false;
+	}
 }
--- ecs_damage.h
+++ ecs_damage.h
@@ -2,6 +2,30 @@
 
 namespace ecs
 {
+	enum class DamageType
+	{
+		Default,
+		Melee,
+		Projectile,
+		Explosion,
+		//Magic,
+		//Environment,
+	};
 
+	struct Damage
+	{
+		DamageType type = DamageType::Default;
+		int amount = 0;
+		//sf::Vector2f direction; //Could be useful for knockback
+	};
+
+	// Delegates to the appropriate function based on the entity's class (e.g. player, enemy, etc.)
+	// Returns true if the entity was damaged, false otherwise.
+	bool apply_damage(entt::entity entity, const Damage& damage);
+
+	// CLASS-SPECIFIC DAMAGE FUNCTIONS (don't call these directly, use apply_damage() instead!)
+
+	bool apply_damage_to_player(entt::entity entity, const Damage& damage);
+	bool apply_damage_to_grass(entt::entity entity, const Damage& damage);
 }
 
