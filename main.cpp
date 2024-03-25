ee826961b42831500746b1650228be069959734c
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-03-25 21:24:41 +0100

    Small refactor

--- main.cpp
+++ main.cpp
@@ -177,8 +177,8 @@ int main(int argc, char* argv[])
             ImGui::PlotLines("##fps", fps_buffer, 256, buffer_offset, overlay_text, 0.f, 400.f, ImVec2(0, 80));
             ImGui::Value("Sprites Drawn", sprites::get_sprites_drawn());
             ImGui::Value("Batches Drawn", sprites::get_batches_drawn());
-            ImGui::Value("Largest Batch Size", sprites::get_sprites_in_largest_batch());
-            ImGui::Checkbox("Sprite Batching", &sprites::enable_batching);
+            ImGui::Value("Largest Batch", sprites::get_sprites_in_largest_batch());
+            ImGui::Checkbox("Enable Batching", &sprites::enable_batching);
             ImGui::End();
         }
 
