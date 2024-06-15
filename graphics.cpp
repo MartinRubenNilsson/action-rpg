741f04b5c0a7426771dc1c59d1312aa613660d12
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-06-15 13:33:21 +0200

    Write code for loading and setting shaders

--- graphics.cpp
+++ graphics.cpp
@@ -12,13 +12,20 @@ namespace graphics
 		std::filesystem::path fragment_shader_path;
 		std::string vertex_shader_bytecode;
 		std::string fragment_shader_bytecode;
-		int vertex_shader_id = -1;
-		int fragment_shader_id = -1;
-		//TODO
+		uint32_t vertex_shader_object = 0;
+		uint32_t fragment_shader_object = 0;
+		uint32_t program_object = 0;
 	};
 
 	std::vector<Shader> _shaders;
 	std::unordered_map<std::string, int> _shader_handle_to_id;
+
+	Shader* shader_get(int shader_id)
+	{
+		if (shader_id < 0 || shader_id >= (int)_shaders.size())
+			return nullptr;
+		return &_shaders[shader_id];
+	}
 
 	int shader_load(const std::filesystem::path& vertex_shader_path, const std::filesystem::path& fragment_shader_path)
 	{
@@ -30,6 +37,8 @@ namespace graphics
 			return it->second;
 		}
 
+		int success;
+
 		// LOAD AND CREATE VERTEX SHADER
 
 		std::ifstream vertex_shader_file{ vertex_shader_path };
@@ -38,9 +47,73 @@ namespace graphics
 			return -1;
 		}
 		std::string vertex_shader_bytecode{ std::istreambuf_iterator<char>(vertex_shader_file), std::istreambuf_iterator<char>() };
-		const int vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+		const int vertex_shader_object = glCreateShader(GL_VERTEX_SHADER);
+		const char* vertex_shader_bytecode_cstr = vertex_shader_bytecode.c_str();
+		glShaderSource(vertex_shader_object, 1, &vertex_shader_bytecode_cstr, nullptr);
+		glCompileShader(vertex_shader_object);
+		glGetShaderiv(vertex_shader_object, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			char info_log[512];
+			glGetShaderInfoLog(vertex_shader_object, sizeof(info_log), nullptr, info_log);
+			console::log_error("Failed to compile vertex shader: " + vertex_shader_path.string() + "\n" + info_log);
+			return -1;
+		}
 
-		return -1;
+		// LOAD AND CREATE FRAGMENT SHADER
+
+		std::ifstream fragment_shader_file{ fragment_shader_path };
+		if (!fragment_shader_file) {
+			console::log_error("Failed to open fragment shader file: " + fragment_shader_path.string());
+			return -1;
+		}
+		std::string fragment_shader_bytecode{ std::istreambuf_iterator<char>(fragment_shader_file), std::istreambuf_iterator<char>() };
+		const int fragment_shader_object = glCreateShader(GL_FRAGMENT_SHADER);
+		const char* fragment_shader_bytecode_cstr = fragment_shader_bytecode.c_str();
+		glShaderSource(fragment_shader_object, 1, &fragment_shader_bytecode_cstr, nullptr);
+		glCompileShader(fragment_shader_object);
+		glGetShaderiv(fragment_shader_object, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			char info_log[512];
+			glGetShaderInfoLog(fragment_shader_object, sizeof(info_log), nullptr, info_log);
+			console::log_error("Failed to compile fragment shader: " + fragment_shader_path.string() + "\n" + info_log);
+			return -1;
+		}
+
+		// CREATE PROGRAM OBJECT
+
+		const int program_object = glCreateProgram();
+		glAttachShader(program_object, vertex_shader_object);
+		glAttachShader(program_object, fragment_shader_object);
+		glLinkProgram(program_object);
+		glGetProgramiv(program_object, GL_LINK_STATUS, &success);
+		if (!success) {
+			char info_log[512];
+			glGetProgramInfoLog(program_object, sizeof(info_log), nullptr, info_log);
+			console::log_error("Failed to link program object: " + vertex_shader_path.string() + ", " +
+				fragment_shader_path.string() + "\n" + info_log);
+			return -1;
+		}
+
+		// STORE SHADER
+
+		const int shader_id = (int)_shaders.size();
+		Shader& shader = _shaders.emplace_back();
+		shader.vertex_shader_path = vertex_shader_path;
+		shader.fragment_shader_path = fragment_shader_path;
+		shader.vertex_shader_bytecode = std::move(vertex_shader_bytecode);
+		shader.fragment_shader_bytecode = std::move(fragment_shader_bytecode);
+		shader.vertex_shader_object = vertex_shader_object;
+		shader.fragment_shader_object = fragment_shader_object;
+		shader.program_object = program_object;
+		_shader_handle_to_id[shader_handle] = shader_id;
+
+		return shader_id;
 	}
 
+	void shader_use(int shader_id)
+	{
+		if (const Shader* shader = shader_get(shader_id)) {
+			glUseProgram(shader->program_object);
+		}
+	}
 }
--- graphics.h
+++ graphics.h
@@ -4,4 +4,5 @@ namespace graphics
 {
 	//Returns -1 on failure.
 	int shader_load(const std::filesystem::path& vertex_shader_path, const std::filesystem::path& fragment_shader_path);
+	void shader_use(int shader_id);
 }
