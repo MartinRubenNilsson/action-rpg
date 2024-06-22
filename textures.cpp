b2aa8158ecbdb1b5791a03c089bf42b0acabf60c
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-06-22 14:28:49 +0200

    Remove unused stuff from textures.h/.cpp

--- textures.cpp
+++ textures.cpp
@@ -5,54 +5,14 @@
 
 namespace textures
 {
-	extern bool log_errors =
-#ifdef _DEBUG
-		true;
-#else
-		false;
-#endif
-
-	struct TextureAsset
-	{
-		enum State
-		{
-			NotLoaded,
-			Loaded,
-			Error
-		};
-
-		std::shared_ptr<sf::Texture> texture;
-		State state = NotLoaded;
-
-		void load(const std::string& filename)
-		{
-			texture = std::make_shared<sf::Texture>();
-			state = texture->loadFromFile(filename) ? Loaded : Error;
-			if (state == Error && log_errors)
-				console::log_error("Failed to load texture: " + filename);
-		}
-	};
-
-	std::shared_ptr<sf::Texture> _error_texture;
-	std::unordered_map<std::filesystem::path, TextureAsset> _texture_cache;
 	std::vector<std::unique_ptr<sf::RenderTexture>> _render_texture_pool;
 
 	void shutdown()
 	{
-		_error_texture.reset();
-		clear_texture_cache();
 		clear_render_texture_pool();
 	}
 
-	std::shared_ptr<sf::Texture> get_error_texture()
-	{
-		if (!_error_texture)
-			_error_texture = create_checkerboard_texture(
-				{ 512, 512 }, { 64, 64 }, sf::Color::Black, sf::Color::Magenta);
-		return _error_texture;
-	}
-
-	std::shared_ptr<sf::Texture> create_checkerboard_texture(
+	/*std::shared_ptr<sf::Texture> create_checkerboard_texture(
 		const sf::Vector2u& size, const sf::Vector2u& tile_size, const sf::Color& color1, const sf::Color& color2)
 	{
 		const int shader_id = graphics::load_shader({}, "assets/shaders/checkerboard.frag");
@@ -70,23 +30,7 @@ namespace textures
 		render_texture.display();
 		graphics::bind_shader(-1);
 		return std::make_shared<sf::Texture>(render_texture.getTexture());
-	}
-
-	std::shared_ptr<sf::Texture> load_cached_texture(const std::filesystem::path& path)
-	{
-		std::filesystem::path normal_path = path.lexically_normal();
-		TextureAsset& asset = _texture_cache[normal_path];
-		if (asset.state == TextureAsset::NotLoaded) {
-			asset.load(normal_path.string());
-			if (asset.state == TextureAsset::Error)
-				asset.texture = get_error_texture();
-		}
-		return asset.texture;
-	}
-
-	void clear_texture_cache() {
-		_texture_cache.clear();
-	}
+	}*/
 
 	std::unique_ptr<sf::RenderTexture> take_render_texture_from_pool(const sf::Vector2u& size)
 	{
--- textures.h
+++ textures.h
@@ -6,16 +6,6 @@ namespace textures
 
 	void shutdown();
 
-	std::shared_ptr<sf::Texture> get_error_texture();
-	std::shared_ptr<sf::Texture> create_checkerboard_texture(
-		const sf::Vector2u& size, const sf::Vector2u& tile_size, const sf::Color& color1, const sf::Color& color2);
-
-	// TEXTURE CACHE
-
-	// Loads a texture from file, or returns a cached version if it has already been loaded.
-	std::shared_ptr<sf::Texture> load_cached_texture(const std::filesystem::path& path);
-	void clear_texture_cache();
-
 	// RENDER TEXTURE POOL
 
 	// Searches the pool for a render texture of the specified size and returns it,
