0b5e9a9ef11125ba217a8235943fe80a364489a4
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-06-22 14:23:35 +0200

    Render cursor using sprite system

--- cursor.cpp
+++ cursor.cpp
@@ -1,10 +1,10 @@
 #include "stdafx.h"
 #include "cursor.h"
-#include "textures.h"
+#include "graphics.h"
+#include "sprites.h"
 
 namespace cursor
 {
-	const std::filesystem::path _CURSOR_TEXTURE_PATH = "assets/textures/cursors/cursor.png";
 	bool _visible = true;
 	sf::IntRect _texture_rect{ 0, 0, 16, 16 };
 	sf::Vector2f _position;
@@ -13,12 +13,22 @@ namespace cursor
 	void render(sf::RenderTarget& target)
 	{
 		if (!_visible) return;
-		std::shared_ptr<sf::Texture> texture = textures::load_cached_texture(_CURSOR_TEXTURE_PATH);
-		if (!texture) return;
-		sf::Sprite sprite(*texture, _texture_rect);
-		sprite.setPosition(_position);
-		sprite.setScale(_scale, _scale);
-		target.draw(sprite);
+
+		const int texture_id = graphics::load_texture("assets/textures/cursors/cursor.png");
+		if (texture_id == -1) return;
+		unsigned int texture_width, texture_height;
+		graphics::get_texture_size(texture_id, texture_width, texture_height);
+
+		sprites::Sprite sprite{};
+		sprite.texture_id = texture_id;
+		sprite.min = _position;
+		sprite.max = _position + _scale * sf::Vector2f{ 16.f, 16.f };
+		sprite.tex_min = { (float)_texture_rect.left, (float)_texture_rect.top };
+		sprite.tex_max = { (float)_texture_rect.left + _texture_rect.width, (float)_texture_rect.top + _texture_rect.height };
+		sprite.tex_min /= sf::Vector2f{ (float)texture_width, (float)texture_height };
+		sprite.tex_max /= sf::Vector2f{ (float)texture_width, (float)texture_height };
+		sprites::draw(sprite);
+		sprites::render(target);
 	}
 
 	void set_visible(bool visible) {
