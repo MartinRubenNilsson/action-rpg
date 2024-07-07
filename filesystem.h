cc56c6466cba7b5a93f7283248ee13322c8850c5
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-07-07 14:02:48 +0200

    Add some known file types to enum

--- filesystem.h
+++ filesystem.h
@@ -2,6 +2,19 @@
 
 namespace filesystem
 {
+	//These are not used for anything rn
+	enum class FileType
+	{
+		Unknown,
+		VertexShader,
+		FragmentShader,
+		FmodStudioBank,
+		TiledMap,
+		TiledTileset,
+		TiledTemplate,
+		TrueTypeFont,
+	};
+
 	std::string get_normalized_path(const std::string& path);
 	std::string get_parent_path(const std::string& path);
 	std::string get_stem(const std::string& path);
