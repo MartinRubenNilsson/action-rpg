06d9220f76ffc8d9b09b70c225558f3c51935489
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-07-07 14:06:55 +0200

    Add some funcs

--- filesystem.cpp
+++ filesystem.cpp
@@ -14,8 +14,18 @@ namespace filesystem
 		return std::filesystem::path(path).parent_path().string();
 	}
 
+	std::string get_filename(const std::string& path)
+	{
+		return std::filesystem::path(path).filename().string();
+	}
+
 	std::string get_stem(const std::string& path)
 	{
 		return std::filesystem::path(path).stem().string();
+	}
+
+	std::string get_extension(const std::string& path)
+	{
+		return std::filesystem::path(path).extension().string();
 	}
 }
--- filesystem.h
+++ filesystem.h
@@ -17,5 +17,7 @@ namespace filesystem
 
 	std::string get_normalized_path(const std::string& path);
 	std::string get_parent_path(const std::string& path);
+	std::string get_filename(const std::string& path);
 	std::string get_stem(const std::string& path);
+	std::string get_extension(const std::string& path);
 }
