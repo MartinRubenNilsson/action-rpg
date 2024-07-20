ebfeb2df67348832263fd43abaa27814415a39ba
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-07-20 14:39:46 +0200

    Add colliders to some objects in map

--- output/assets/tiled/maps/23.07a - Eternal Dungeon/TSX files/eternal dungeon 16x32.tsx
+++ output/assets/tiled/maps/23.07a - Eternal Dungeon/TSX files/eternal dungeon 16x32.tsx
@@ -1,6 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="eternal dungeon 16x32" tilewidth="16" tileheight="32" tilecount="8" columns="4">
  <image source="../eternal dungeon objects/eternal dungeon 16x32.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-0.0625" y="18.0625" width="16" height="13.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0625" y="18.625" width="15.875" height="13.1875"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="17.875" width="16.3125" height="14"/>
+  </objectgroup>
+ </tile>
  <tile id="4">
   <animation>
    <frame tileid="4" duration="240"/>
