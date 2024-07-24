c72c32c548fc0fabf3d5cbcae02d3d6b2da8b627
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2024-07-24 12:10:28 +0200

    Add treasure chest tileset

---
+++ output/assets/tiled/tilesets/props/treasure_chests/treasure_chests.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="treasure_chests" tilewidth="32" tileheight="32" tilecount="30" columns="5">
+ <image source="treasure_chests.png" width="160" height="192"/>
+</tileset>
