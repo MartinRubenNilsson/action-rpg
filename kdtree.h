c3b714cc8d27494e69f2cc9072d92f7d5e2ec6e1
Martin Nilsson <martin.ruben.nilsson@gmail.com>
2025-06-11 13:08:20 +0200

    Comments

--- kdtree.h
+++ kdtree.h
@@ -3,8 +3,12 @@
 #include <span>
 
 namespace kdtree {
-	// Complexity: O(n log^2 n)
+	// Builds a k-d tree from a set of points in 2D space. Complexity: O(n log^2 n)
 	void build_by_sorting(std::span<Vector2f> kdtree);
-	// Complexity: O(log n)
+
+	// Queries the nearest point to a given point in the k-d tree. Sets 'nearest_index' to the index of the nearest point
+	// in the k-d tree, and 'nearest_distance_sq' to the squared distance to that point. You can set 'nearest_distance_sq'
+	// < FLT_MAX to restrict the search to points within a certain distance. Sets 'nearest_index' to SIZE_MAX if no point
+	// is found within the distance, or if the tree is empty. Complexity: O(log n)
 	void query_nearest(std::span<const Vector2f> kdtree, const Vector2f& point, size_t& nearest_index, float& nearest_distance_sq);
 }
